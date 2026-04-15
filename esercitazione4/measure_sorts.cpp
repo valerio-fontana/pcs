ad35e3ec072bc871d5b589de2ab70117e3f4bd22
Valerio Fontana <s322612@studenti.polito.it>
2026-04-15 17:26:57 +0000

    modifica al measure_sorts

--- esercitazione4/measure_sorts.cpp
+++ esercitazione4/measure_sorts.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
 #include "sorts.hpp"
 #include "randfiller.h"
@@ -13,38 +14,53 @@ int main()
     randfiller rf;
     timecounter tc;
 
-    for (int i = 4; i <= 8192; i *= 2) {
-        vector<int> v(i);
+    ofstream out("times.dat");
+
+    if (!out) {
+        cerr << "Errore nell'apertura del file.\n";
+        return 1;
+    }
+
+    for (int size = 4; size <= 8192; size *= 2) {
+        vector<int> v(size);
 
         rf.fill(v, -100, 100);
-
-        vector<int> v_bubble = v;
-        vector<int> v_insertion = v;
-        vector<int> v_selection = v;
-        vector<int> v_std = v;
-
         tc.tic();
-        bubble_sort(v_bubble);
+        bubble_sort(v);
         double t_bubble = tc.toc();
 
+        rf.fill(v, -100, 100);
         tc.tic();
-        insertion_sort(v_insertion);
+        insertion_sort(v);
         double t_insertion = tc.toc();
 
+        rf.fill(v, -100, 100);
         tc.tic();
-        selection_sort(v_selection);
+        selection_sort(v);
         double t_selection = tc.toc();
 
+        rf.fill(v, -100, 100);
         tc.tic();
-        sort(v_std.begin(), v_std.end());
+        sort(v.begin(), v.end());
         double t_std = tc.toc();
 
-        cout << "Dimensione: " << i << "\n" <<
+        cout << "Dimensione: " << size << "\n" <<
         "Bubble: " << t_bubble << "\n" <<
         "Insertion: " << t_insertion << "\n" <<
         "Selection: " << t_selection << "\n" <<
         "Standard sort: " << t_std << "\n\n";
+
+        out << size << " " <<
+        t_bubble << " " <<
+        t_insertion << " " <<
+        t_selection << " " <<
+        t_std << "\n";
     }
+
+    out.close();
+
+    cout << "Risultati salvati anche in times.dat.\n" <<
+    "Ordine: size, bubble, insertion, selection, std_sort.\n";
 
     return 0;
 }
